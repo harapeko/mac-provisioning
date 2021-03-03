caaf55f3548979e458e7404130b02cc27dd8055f
harapeko <gmail@harapeko.dev>
2021-03-04 02:35:46 +0900

    add install ansible

--- scripts/AnsibleInstall.sh
+++ scripts/AnsibleInstall.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-which pip > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	echo "[already pip]"
-else
-	echo "[install pip]"
-	sudo easy_install pip
-	echo "[installed pip]"
-fi
-
 which ansible > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	echo "[already ansible]"
 else
 	echo "[install ansible]"
-	sudo pip install ansible
+	arch -arm64 brew install ansible
+	zsh
 	echo "[installed ansible]"
 fi
