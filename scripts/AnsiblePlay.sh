c1208e87745f9bfae793247cfe74258d024df98d
harapeko <gmail@harapeko.dev>
2021-03-06 13:25:12 +0900

    add install mas

--- scripts/AnsiblePlay.sh
+++ scripts/AnsiblePlay.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd provisioning > /dev/null 2>&1
-ansible-playbook -i hosts site.yml
+# `--ask-become-pass` is for `xcodebuild -license accept`
+ansible-playbook -i hosts site.yml --ask-become-pass
