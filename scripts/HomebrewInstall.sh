a19f46be3664a488ece48b49167203e98a5d58de
harapeko <gmail@harapeko.dev>
2022-03-29 11:06:56 +0900

    brewインストール後に反映されるようにした

--- scripts/HomebrewInstall.sh
+++ scripts/HomebrewInstall.sh
@@ -8,7 +8,7 @@ else
   echo "[install Homebrew]"
   arch -arm64e /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/harapeko/.zprofile
-  # ↑再起動しても読み込んでくれない？でも手動でシェル再起動してmake再実行すれば動くのでよし
+  eval "$(/opt/homebrew/bin/brew shellenv)"
   exec $SHELL -l
   echo "[installed Homebrew]"
 fi
